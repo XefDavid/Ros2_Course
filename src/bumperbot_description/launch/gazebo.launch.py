824ed9eba568a829f3a9c6116ed49336e997e14f
xefDavid <chefdavid@hotmail.com>
2025-09-26 11:02:52 +0200

    section  control configuration with gazebo,

--- src/bumperbot_description/launch/gazebo.launch.py
+++ src/bumperbot_description/launch/gazebo.launch.py
@@ -11,7 +11,8 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 def generate_launch_description():
     bumperbot_description_dir = get_package_share_directory("bumperbot_description")
-
+    ros_distro = os.environ["ROS_DISTRO"]
+    is_ignition = "True" if ros_distro == "humble" else "False"
 
     model_arg = DeclareLaunchArgument(
         name="model",
@@ -19,7 +20,13 @@ def generate_launch_description():
         description="Absolute path to robot URDF file"
     )
 
-    robot_description = ParameterValue(Command(["xacro ", LaunchConfiguration("model")]), value_type=str)
+    robot_description = ParameterValue(Command([
+        "xacro ",
+          LaunchConfiguration("model"),
+          " is_ignition:=",
+          is_ignition
+          ]), 
+          value_type=str)
 
     robot_state_publisher = Node(
         package="robot_state_publisher",
@@ -38,7 +45,7 @@ def generate_launch_description():
         os.path.join(
             get_package_share_directory("ros_gz_sim"), "launch"),"/gz_sim.launch.py"]),
             launch_arguments=[
-                ("gz_args", [" -v 4"," -r", " empty.sdf"])
+                ("gz_arg", [" -v 4"," -r", " empty.sdf"])
             ]
     )
     gz_spawn_entity = Node(
