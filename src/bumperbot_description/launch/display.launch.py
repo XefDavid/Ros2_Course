aa3a320d8945a01003216a79a678c3e366384a6e
xefDavid <chefdavid@hotmail.com>
2025-10-04 09:04:35 +0200

    fixed code controller terminal and gazebo terminal working finally

--- src/bumperbot_description/launch/display.launch.py
+++ src/bumperbot_description/launch/display.launch.py
@@ -1,29 +1,32 @@
-from launch import LaunchDescription
-from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument
 import os
 from ament_index_python.packages import get_package_share_directory
-from launch_ros.parameter_descriptions import ParameterValue
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
 from launch.substitutions import Command, LaunchConfiguration
 
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+
+
 def generate_launch_description():
+    bumperbot_description_dir = get_package_share_directory("bumperbot_description")
 
+    model_arg = DeclareLaunchArgument(name="model", default_value=os.path.join(
+                                        bumperbot_description_dir, "urdf", "bumperbot.urdf.xacro"
+                                        ),
+                                      description="Absolute path to robot urdf file")
 
-    model_arg = DeclareLaunchArgument(
-        name="model",
-        default_value= os.path.join(get_package_share_directory("bumperbot_description"), "urdf", "bumperbot.urdf.xacro"),
-        description="Absolute path to robot URDF file"
-    )
+    robot_description = ParameterValue(Command(["xacro ", LaunchConfiguration("model")]),
+                                       value_type=str)
 
-    robot_description = ParameterValue(Command(["xacro ", LaunchConfiguration("model")]), value_type=str)
-
-    robot_state_publisher = Node(
+    robot_state_publisher_node = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
         parameters=[{"robot_description": robot_description}]
     )
 
-    joint_state_publisher_gui = Node(
+    joint_state_publisher_gui_node = Node(
         package="joint_state_publisher_gui",
         executable="joint_state_publisher_gui"
     )
@@ -33,11 +36,12 @@ def generate_launch_description():
         executable="rviz2",
         name="rviz2",
         output="screen",
-        arguments=["-d", os.path.join(get_package_share_directory("bumperbot_description"), "rviz","display.rviz")] 
+        arguments=["-d", os.path.join(bumperbot_description_dir, "rviz", "display.rviz")],
     )
-    return LaunchDescription(
-       [ model_arg,
-        robot_state_publisher,
-        joint_state_publisher_gui,
-        rviz_node,        ]
-    )
+
+    return LaunchDescription([
+        model_arg,
+        joint_state_publisher_gui_node,
+        robot_state_publisher_node,
+        rviz_node
+    ])
