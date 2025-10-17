53d86461a6ddfb6d7460e1ece9eb2a69cc0d7beb
xefDavid <chefdavid@hotmail.com>
2025-10-17 13:00:16 +0200

    section kinematics quaternion

--- src/bumperbot_py_examples/bumperbot_py_examples/simple_tf_kinematics.py
+++ src/bumperbot_py_examples/bumperbot_py_examples/simple_tf_kinematics.py
@@ -1,8 +1,13 @@
 import rclpy
 from rclpy.node import Node
 from tf2_ros.static_transform_broadcaster import StaticTransformBroadcaster
-from tf2_ros import TransformBroadcaster
+from tf2_ros import TransformBroadcaster , TransformException
+from tf2_ros.buffer import Buffer
+from tf2_ros.transform_listener import TransformListener
 from geometry_msgs.msg import TransformStamped
+from bumperbot_msgs.srv import GetTransform
+from tf_transformations import quaternion_from_euler, quaternion_multiply, quaternion_inverse
+
 
 
 class SimpleTfKinematics (Node):
@@ -21,6 +26,15 @@ class SimpleTfKinematics (Node):
         #creamos la varibles de incremento en linea recta en este caso
         self.x_increment_ = 0.05
         self.last_x_ = 0.0
+        #Creamos variables para almacenar la rotación
+        self.rotations_counter_ = 0
+        self.last_orientation_ = quaternion_from_euler(0, 0, 0)
+        self.orientation_increment_ =quaternion_from_euler(0,0,0.5)
+
+
+        #creamos dos variables para almacenar las transformacion del buffer y su subscripcion
+        self.tf_buffer_ = Buffer()
+        self.tf_listener_ =TransformListener(self.tf_buffer_, self)
 
         self.static_transform_stamped.header.stamp = self.get_clock().now().to_msg()
         self.static_transform_stamped.header.frame_id = "bumperbot_base"
@@ -43,8 +57,13 @@ class SimpleTfKinematics (Node):
         self.get_logger().info(f"publishing static transform between {self.static_transform_stamped.header.frame_id} and {self.static_transform_stamped.child_frame_id}")
         self.get_logger().info(f"publicando diferencia entre {self.dynamic_transform_stamped_.header.frame_id} and {self.static_transform_stamped.header.frame_id}")
 
-    #creamos una funcion que servirá para controlar cada cuanto se va a publicar los mensajes en el hijo dinamico
+        #Creamos una funcion que servirá para controlar cada cuanto se va a publicar los mensajes en el hijo dinamico
         self.timer_= self.create_timer(0.1,self.timerCallback)
+
+        #Vamos a crear una variable para almacenar el listener
+        # 1º definimos el archivo srv, 2º le damos un nombre al servicio listener, 3º funcion donde vamos a realizar lo que necesitamos 
+        self.get_transform_srv_= self.create_service(GetTransform, "get_transform", self.getTransformCallback)
+
     
     def timerCallback(self):
         self.dynamic_transform_stamped_.header.stamp = self.get_clock().now().to_msg()
@@ -53,14 +72,40 @@ class SimpleTfKinematics (Node):
         self.dynamic_transform_stamped_.transform.translation.x = self.last_x_ +self.x_increment_
         self.dynamic_transform_stamped_.transform.translation.y = 0.0
         self.dynamic_transform_stamped_.transform.translation.z = 0.0
-        self.dynamic_transform_stamped_.transform.rotation.x = 0.0
-        self.dynamic_transform_stamped_.transform.rotation.y = 0.0
-        self.dynamic_transform_stamped_.transform.rotation.z = 0.0
-        self.dynamic_transform_stamped_.transform.rotation.w = 1.0
+        q = quaternion_multiply(self.last_orientation_,self.orientation_increment_)
+        self.dynamic_transform_stamped_.transform.rotation.x = q[0]
+        self.dynamic_transform_stamped_.transform.rotation.y = q[1]
+        self.dynamic_transform_stamped_.transform.rotation.z = q[2]
+        self.dynamic_transform_stamped_.transform.rotation.w = q[3]
 
         self.dynamic_tf_broadcaster_.sendTransform(self.dynamic_transform_stamped_)
 
         self.last_x_ = self.dynamic_transform_stamped_.transform.translation.x
+        self.rotations_counter_ += 1
+        self.last_orientation_ = q
+
+        if self.rotations_counter_ >= 100:
+            self.orientation_increment_= quaternion_inverse(self.orientation_increment_)
+            self.rotations_counter_= 0
+
+    # Definimos la funcion a usar dentro del servicio
+    def getTransformCallback(self, req, res ):
+        #Creamos un log para asegurarnos que va funcionando el flujo
+        self.get_logger().info(f"Requested Transform between {req.frame_id} and  {req.child_frame_id}")
+        #creamos una variable que almacena una valor de fecha y hora
+        requested_transform = TransformStamped()
+        try:
+            #Intentamos almacenar dentro de la varible que devuelve la transformacion entre dos frames
+            requested_transform = self.tf_buffer_.lookup_transform(req.frame_id, req.child_frame_id, rclpy.time.Time())
+            #Si no hay esos datos de transformacion se lanza una excepcion
+        except TransformException as e:
+            self.get_logger().error(f"An error occurred while transforming {req.frame_id} and {req.child_frame_id}")
+            res.success = False
+            return res
+            
+        res.transform = requested_transform
+        res.success = True
+        return res
 
 def main():
     rclpy.init()
