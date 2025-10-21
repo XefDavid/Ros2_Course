393a5e744a01f31b9bca84d437b6e7007e2aab60
xefDavid <chefdavid@hotmail.com>
2025-10-21 10:44:06 +0200

    add lesson 9.104, wheel position

--- src/bumperbot_controller/bumperbot_controller/simple_controller.py
+++ src/bumperbot_controller/bumperbot_controller/simple_controller.py
@@ -5,6 +5,10 @@ from rclpy.node import Node #importamos node para poder crear los nodos
 from std_msgs.msg import Float64MultiArray #asi el mensaje se puede mandar en un array de multiples numeros en 64bits
 from geometry_msgs.msg import TwistStamped #con esta importacion cada mensaje publicado tiene un time stamp
 import numpy as np #importamos numpy para que se pueda realizar operaciones aritmeticas  como las matriz
+from sensor_msgs.msg import JointState
+from rclpy.time import Time
+from rclpy.constants import S_TO_NS #esta libreria es para cambiar los segundos a nanosegundos
+import math
 
 class SimpleController(Node):# creamos la clase del nodo
     def __init__(self): #constructor
@@ -22,10 +26,24 @@ class SimpleController(Node):# creamos la clase del nodo
         self.get_logger().info("Using wheel radius %f" % self.wheel_radius) #forma antigua de escribir un logger
         self.get_logger().info(f"using wheel separation {self.wheel_separation}")#forma moderna 
 
+        #creamos unas nuevas variables para la leccion 9.10
+        #Instanciamos dentro de estas variables la posicion inicial de cada rueda y un time de ese momento actual.
+        self.left_wheel_prev_pos_= 0.0
+        self.right_wheel_prev_pos_= 0.0
+        self.prev_time_= self.get_clock().now()
+
+        #Creamos varibles para almacenar la posicion x , y y theta
+        self.x_ = 0
+        self.y_ = 0
+        self.theta_ = 0.0
+
+
         #creamo una subscripcion al nodo con este topico  en que vamos a escribir un array de decimales, en que va ahaber en cola no mas de diez mensajes
         self.wheel_cmd_pub = self.create_publisher(Float64MultiArray,"simple_velocity_controller/commands", 10)
         #creamos una subscripcion a un topic en el que  se llama a la funcion callback, cada mensaje tiene un timestamp
         self.vel_sub = self.create_subscription(TwistStamped,"bumperbot_controller/cmd_vel",self.velCallback, 10) 
+        #nueva subscripcion para escuchar lo que la libreria ros2 utliza para publicar las current position del robot
+        self.joint_sub_ = self.create_subscription(JointState,"joint_states", self.jointCallback, 10)
 
         #creamos una matriz de 2x2 para cinematica
         self.speed_conversion = np.array([[self.wheel_radius/2, self.wheel_radius/2],# calcula la velocidad lineal riueda derecha e izquierda de forma respectiva
@@ -43,6 +61,31 @@ class SimpleController(Node):# creamos la clase del nodo
         wheel_speed_msg.data = [wheel_speed[0,0],wheel_speed[1,0]]
 
         self.wheel_cmd_pub.publish(wheel_speed_msg)#publica en el topic wheel_cmd_pub la velocidad 
+
+    def jointCallback(self,msg):
+        dp_left = msg.position[1] - self.left_wheel_prev_pos_
+        dp_right = msg.position[0] - self.right_wheel_prev_pos_  
+        dt = Time.from_msg(msg.header.stamp) - self.prev_time_
+
+        self.left_wheel_prev_pos_ = msg.position[1]
+        self.right_wheel_prev_pos_ = msg.position[0]
+        self.prev_time_ = Time.from_msg(msg.header.stamp)
+
+        fi_left = dp_left / (dt.nanoseconds / S_TO_NS)
+        fi_right = dp_right / (dt.nanoseconds / S_TO_NS)
+
+        linear = (self.wheel_radius * fi_right + self.wheel_radius * fi_left) / 2
+        angular = (self.wheel_radius * fi_right - self.wheel_radius * fi_left) / self.wheel_separation
+
+        #Creamos varibles para almacenar el incremento
+        d_s = (self.wheel_radius * dp_right + self.wheel_radius * dp_left) / 2
+        d_theta = (self.wheel_radius * dp_right - self.wheel_radius * dp_left) / self.wheel_separation
+        self.theta_ += d_theta
+        self.x_ += d_s * math.cos(self.theta_)
+        self.y_ += d_s * math.sin(self.theta_) 
+
+        self.get_logger().info(f"linear vel:{linear}, angular vel:{angular}")
+        self.get_logger().info(f"Posicion x =>{self.x_}, Posicion y =>{self.y_}, orientation theta =>{self.theta_}")
 
 def main():
     rclpy. init()
