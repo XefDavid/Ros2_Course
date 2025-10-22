06a41f1fe1385261170aea5dc1ea3fce24b0732c
xefDavid <chefdavid@hotmail.com>
2025-10-22 12:52:20 +0200

    adaptaions

--- src/bumperbot_controller/bumperbot_controller/simple_controller.py
+++ src/bumperbot_controller/bumperbot_controller/simple_controller.py
@@ -9,6 +9,8 @@ from sensor_msgs.msg import JointState
 from rclpy.time import Time
 from rclpy.constants import S_TO_NS #esta libreria es para cambiar los segundos a nanosegundos
 import math
+from nav_msgs.msg import Odometry
+from tf_transformations import quaternion_from_euler
 
 class SimpleController(Node):# creamos la clase del nodo
     def __init__(self): #constructor
@@ -44,11 +46,22 @@ class SimpleController(Node):# creamos la clase del nodo
         self.vel_sub = self.create_subscription(TwistStamped,"bumperbot_controller/cmd_vel",self.velCallback, 10) 
         #nueva subscripcion para escuchar lo que la libreria ros2 utliza para publicar las current position del robot
         self.joint_sub_ = self.create_subscription(JointState,"joint_states", self.jointCallback, 10)
+        #Creamos un nuvo nodo para publicar los mensajes con Odometria
+        self.odom_pub_ = self.create_publisher(Odometry, "bumperbot_controller/odom",10)
 
         #creamos una matriz de 2x2 para cinematica
         self.speed_conversion = np.array([[self.wheel_radius/2, self.wheel_radius/2],# calcula la velocidad lineal riueda derecha e izquierda de forma respectiva
                                           [self.wheel_radius/self.wheel_separation, -self.wheel_radius/self.wheel_separation] #calcula la rotacion(velocidad angular)
                                           ])
+        #Creamos una nuevas varibles que va a servir para implementar el mensaje que queremos guardar para el topic "bumperbot_controller/odom"
+        self.odom_msg_ = Odometry()
+        self.odom_msg_.header.frame_id = "odom"
+        self.odom_msg_.child_frame_id = "base_footprint"
+        self.odom_msg_.pose.pose.orientation.x = 0.0     
+        self.odom_msg_.pose.pose.orientation.y = 0.0   
+        self.odom_msg_.pose.pose.orientation.z = 0.0   
+        self.odom_msg_.pose.pose.orientation.w = 1.0   
+
         self.get_logger().info(f"the conversion matrix is {self.speed_conversion}")
     
     def velCallback(self, msg):
@@ -84,8 +97,23 @@ class SimpleController(Node):# creamos la clase del nodo
         self.x_ += d_s * math.cos(self.theta_)
         self.y_ += d_s * math.sin(self.theta_) 
 
-        self.get_logger().info(f"linear vel:{linear}, angular vel:{angular}")
-        self.get_logger().info(f"Posicion x =>{self.x_}, Posicion y =>{self.y_}, orientation theta =>{self.theta_}")
+        #Cuando llegan los datos de las ruedas
+        q = quaternion_from_euler(0, 0, self.theta_)
+        self.odom_msg_.pose.pose.orientation.x = q[0]
+        self.odom_msg_.pose.pose.orientation.y = q[1]
+        self.odom_msg_.pose.pose.orientation.z = q[2]
+        self.odom_msg_.pose.pose.orientation.w = q[3]
+        self.odom_msg_.header.stamp = self.get_clock().now().to_msg()
+        self.odom_msg_.pose.pose.position.x = self.x_
+        self.odom_msg_.pose.pose.position.y = self.y_
+        self.odom_msg_.twist.twist.linear.x = linear
+        self.odom_msg_.twist.twist.angular.z = angular
+
+        # self.get_logger().info(f"linear vel:{linear}, angular vel:{angular}")
+        # self.get_logger().info(f"Posicion x =>{self.x_}, Posicion y =>{self.y_}, orientation theta =>{self.theta_}")
+
+        #publicamos los mensajes en el topic
+        self.odom_pub_.publish(self.odom_msg_)
 
 def main():
     rclpy. init()
--- src/bumperbot_py_examples/setup.py
+++ src/bumperbot_py_examples/setup.py
@@ -27,6 +27,7 @@ setup(
                     'simple_tf_kinematics = bumperbot_py_examples.simple_tf_kinematics:main',                    
                     'simple_service_server = bumperbot_py_examples.simple_service_server:main',
                     'simple_service_client = bumperbot_py_examples.simple_service_client:main',
+                    
 
         ],
     },
